ae79d6d5213a947291fcc1aef3ba9ca5709bb481
c.cortez <c.cortez@uniandes.edu.co>
2024-03-11 20:00:37 -0500

    Add custom dialog

--- app/src/main/java/com/example/mobileprototype/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/mobileprototype/ui/notifications/NotificationsFragment.kt
@@ -1,12 +1,20 @@
 package com.example.mobileprototype.ui.notifications
 
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.mobileprototype.R
 import com.example.mobileprototype.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
@@ -28,11 +36,40 @@ class NotificationsFragment : Fragment() {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+
         val textView: TextView = binding.textNotifications
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        val btn_test = binding.btnTestDialog
+        btn_test.setOnClickListener{
+            val message: String = "Test msg"
+            showCustomDialog(message)
+        }
+
+
         return root
+    }
+
+    private fun showCustomDialog(message: String){
+        var dialog = context?.let { Dialog(it) }
+        dialog!!.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setCancelable(false)
+        dialog.setContentView(R.layout.layout_custom_dialog)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val tvMessage : TextView = dialog.findViewById(R.id.custom_dialog_message)
+        var okButton : Button = dialog.findViewById(R.id.cd_ok_button)
+
+        //change msg if wanted
+        //tvMessage.text = message
+        okButton.setOnClickListener{
+            Toast.makeText(context, "clicked ok", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 
     override fun onDestroyView() {
